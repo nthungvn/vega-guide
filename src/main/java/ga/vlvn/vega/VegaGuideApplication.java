b26e049286a50d36009b2288d9204021b013a0d4
Hung Nguyen <nthung.vlvn@gmail.com>
2022-08-29 23:36:48 +0700

    setup spring project

---
+++ src/main/java/ga/vlvn/vega/VegaGuideApplication.java
@@ -0,0 +1,11 @@
+package ga.vlvn.vega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegaGuideApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VegaGuideApplication.class, args);
+    }
+}
